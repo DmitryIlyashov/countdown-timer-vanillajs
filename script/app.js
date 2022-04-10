f53d6b0a64408c9f7d789b609dad5f29df3ed1e4
Dmitriy Ilyashov <ilyashov.ds@gmail.com>
2022-04-10 13:45:50 +0300

    Created example with timer in format hhmmss

--- script/app.js
+++ script/app.js
@@ -15,27 +15,67 @@ function displayNumberOnDigit(number, digitId) {
     });
 }
 
-function displaySeconds(seconds) {
-    let secondsStr = seconds + '';
-    if (secondsStr.length === 1) {
-        secondsStr = '0' + secondsStr;
-    }
-    displayNumberOnDigit(+secondsStr[0], 'seconds-high-digit');
-    displayNumberOnDigit(+secondsStr[1], 'seconds-low-digit');
+function getFirstDigitOfNumber(number) {
+    let numberStr = ((number + '').length > 1) ? number + '' : '0' + number;
+    return numberStr[0];
 }
 
+function getSecondDigitOfNumber(number) {
+    let numberStr = ((number + '').length > 1) ? number + '' : '0' + number;
+    return numberStr[1];
+}
+
+function displaySeconds(seconds) {
+    displayNumberOnDigit(getFirstDigitOfNumber(seconds), 'seconds-high-digit');
+    displayNumberOnDigit(getSecondDigitOfNumber(seconds), 'seconds-low-digit');
+}
+
+function displayMinutes(minutes) {
+    displayNumberOnDigit(getFirstDigitOfNumber(minutes), 'minutes-high-digit');
+    displayNumberOnDigit(getSecondDigitOfNumber(minutes), 'minutes-low-digit');
+}
+
+function displayHours(hours) {
+    displayNumberOnDigit(getFirstDigitOfNumber(hours), 'hours-high-digit');
+    displayNumberOnDigit(getSecondDigitOfNumber(hours), 'hours-low-digit');
+}
+
+function getSecondsPart(numberOfSeconds) {
+    return numberOfSeconds % 60;
+}
+
+function getMinutesPart(numberOfSeconds) {
+    return ((numberOfSeconds % 3600) - (numberOfSeconds % 60)) / 60;
+}
+
+function getHoursPart(numberOfSeconds) {
+    return (numberOfSeconds - (numberOfSeconds % 3600)) / 3600;
+}
+
+const j = 7302;
+
+console.log(getHoursPart(j));
+console.log(getMinutesPart(j));
+console.log(getSecondsPart(j));
+
+
 function start() {
-    let i = 60;
-    displaySeconds(i);
-    const interval = setInterval(() => {
-        displaySeconds(i);
-        i--;
-        if (i < 0) {
-            clearInterval(interval);
-        }
-    }, 1000);
+    let i = 12384;
+    displayHours(getHoursPart(i));
+    displayMinutes(getMinutesPart(i));
+    displaySeconds(getSecondsPart(i));
+    const interval = setInterval(
+        () => {
+            displayHours(getHoursPart(i));
+            displayMinutes(getMinutesPart(i));
+            displaySeconds(getSecondsPart(i));
+            i--;
+            if (i < 0) {
+                clearInterval(interval);
+            }
+        }, 1000
+    );
 }
 
 start();
-
 
